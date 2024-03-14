d252ece2e3b612278e02daf9624e4dbae97e2513
Сергей <sosnovskysy@yandex.ru>
2024-03-14 10:05:54 +0300

    add controller test

--- src/test/java/dev/sosnovsky/applications/controller/UserControllerTest.java
+++ src/test/java/dev/sosnovsky/applications/controller/UserControllerTest.java
@@ -15,8 +15,6 @@ import org.springframework.http.MediaType;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
